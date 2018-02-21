8d3506e70cb31cc9a70f18b210e8fec179ffbf04
MigoMueller <m.mueller@astro.rug.nl>
2018-02-21 17:02:08 +0100

    Case change in setup.py

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
     description='A Python Toolbox for Bayes-enhanched fitting.',
     keywords = ['Bayesian', 'modeling', 'evidence', 'statistics', 'analysis',
                 'optimization','nested sampling'],
-#    packages=['bayesicFitting/source', 'bayesicFitting/source/kernels', 'bayesicFitting/test', 'bayesicFitting/examples'],
+#    packages=['BayesicFitting/source', 'BayesicFitting/source/kernels', 'BayesicFitting/test', 'BayesicFitting/examples'],
 #    packages = find_packages( exclude=('test',) ),
-    packages=['bayesicFitting', 'bayesicFitting/source', 'bayesicFitting/test', 'bayesicFitting/examples'],
+    packages=['BayesicFitting', 'BayesicFitting/source', 'BayesicFitting/test', 'BayesicFitting/examples'],
     classifiers = [
         "Programming Language :: Python :: 3.5",
         "Operating System :: OS Independent",
