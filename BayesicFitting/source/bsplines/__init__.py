6802144f583b9f2ac03fe382326f24d2f618a300
Do Kester <do@mac-506.local>
2018-03-01 12:30:05 +0100

    bsplines

---
+++ BayesicFitting/source/bsplines/__init__.py
@@ -0,0 +1,19 @@
+#
+#  This file is part of the BayesicFitting package.
+#
+#  BayesicFitting is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as
+#  published by the Free Software Foundation, either version 3 of
+#  the License, or (at your option) any later version.
+#
+#  BayesicFitting is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  The GPL3 license can be found at <http://www.gnu.org/licenses/>.
+#
+
+from .bspline import bspline
+from .splinelab import splinelab
+
