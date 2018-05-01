ee631633e143f1f77f20bfe7b91b950b970771e0
Do Kester <do@mac-506.local>
2018-05-01 13:15:44 +0200

    VoigtModel got partial derivatives

--- BayesicFitting/source/VoigtModel.py
+++ BayesicFitting/source/VoigtModel.py
@@ -1,7 +1,9 @@
 import numpy as numpy
 import math
 from scipy import special
+
 from .NonLinearModel import NonLinearModel
+from . import Tools
 
 __author__ = "Do Kester"
 __year__ = 2018
@@ -80,7 +82,7 @@ class VoigtModel( NonLinearModel ):
         Returns the result of the model function.
 
         Note: both width in the parameter array ( items 2 & 3 ) are kept
-        strictly positive. I.e. they are changed when upon xdata they are negative.
+        strictly positive. I.e. they are changed when upon input they are negative.
 
         Parameters
         ----------
@@ -90,13 +92,127 @@ class VoigtModel( NonLinearModel ):
             values for the parameters.
 
         """
-        x = xdata - params[1]
         sigma = params[2] * math.sqrt( 2.0 )
         gamma = params[3]
+        z0 = 1j * gamma / sigma
+        z = ( xdata - params[1] ) / sigma + z0
 
-        return params[0] * ( numpy.real( special.wofz( ( x + 1j * gamma ) / sigma ) ) /
-                 ( sigma * math.sqrt( math.pi ) ) )
 
+        wofz0 = numpy.real( special.wofz( z0 ) )
+        return params[0] * numpy.real( special.wofz( z ) ) / wofz0
+
+    def basePartial( self, xdata, params, parlist=None ):
+        """
+        Returns the partials at the input value.
+        ..math::
+            z = ( x - p1 + 1j * p3 ) / ( p2 * sqrt2 )
+            z0 = 1j * p3 / ( p2 * sqrt2 )
+
+            vgt = p0 * R( wofzz ) / R( wofz0 )
+
+            dvdp = dvdz * dzdp
+
+            dvdz = p0 * ( R(dwdz) * R(wofz0) - R(dwd0) * R(wofzz) ) / R(wofz0)^2
+            dvdp = p0 * ( R(dwdz * dzdp) * R(wofz0) - R(dwd0 * d0dp) * R(wofzz) ) / R(wofz0)^2
+
+            dwdz = 2j / sqrt(pi) - 2 * z  * wofzz
+            dwd0 = 2j / sqrt(pi) - 2 * z0 * wofz0
+
+
+            ## p0 and p1 have no influence in wofz0
+            dzdp0 = 0
+            dzdp1 = -1 / ( p2 * sqrt2 )
+            d0dp2 = - ( 1j * p3 / ( p2^2 * sqrt2 )              = -z0 / p2
+            dzdp2 = - ( ( x - p1 + 1j * p3 ) / ( p2^2 * sqrt2 ) = -z  / p2
+            dzdp3 = d0dp3 = 1j / ( p2 * sqrt2 )
+
+
+            dvdp0 = R(wofzz) / R(wofz0)
+            ## The other partial follow from calculating dvdp for the parameters 1..3
+
+
+        Parameters
+        ----------
+        xdata : array_like
+            values at which to calculate the partials
+        params : array_like
+            values for the parameters.
+        parlist : array_like
+            list of indices active parameters (or None for all)
+
+        """
+        partial = numpy.ndarray( ( Tools.length( xdata ), self.npbase ) )
+
+        sigma = params[2] * math.sqrt( 2.0 )
+        gamma = params[3]
+        z0 = 1j * gamma / sigma
+        z = ( xdata - params[1] ) / sigma + z0
+
+        wofz0 = special.wofz( z0 )
+        wofzz = special.wofz( z )
+        dwd0 = 2j / math.sqrt( math.pi ) - 2 * z0 * wofz0
+        dwdz = 2j / math.sqrt( math.pi ) - 2 * z * wofzz
+
+        rwz = numpy.real( wofzz )
+        rw0 = numpy.real( wofz0 )
+        a = params[0] / rw0
+        a2 = a / rw0
+
+        dzdp1 = -1 / sigma
+        d0dp2 = -z0 / params[2]
+        dzdp2 = -z / params[2]
+        dzdp3 = 1j / sigma
+
+        parts = { 0 : ( lambda: rwz / rw0 ),
+                  1 : ( lambda: a * numpy.real( dwdz * dzdp1 ) ),
+                  2 : ( lambda: a2 * ( numpy.real( dwdz * dzdp2 ) * rw0 -
+                                       numpy.real( dwd0 * d0dp2 ) * rwz ) ),
+                  3 : ( lambda: a2 * ( numpy.real( dwdz * dzdp3 ) * rw0 -
+                                       numpy.real( dwd0 * dzdp3 ) * rwz ) ) }
+
+        if parlist is None :
+            parlist = range( self.npmax )
+
+        for k,kp in enumerate( parlist ) :
+            partial[:,k] = parts[kp]()
+
+        return partial
+
+    def baseDerivative( self, xdata, params ) :
+        """
+        Return the derivative df/dx at each xdata (=x).
+
+        ..math::
+            z = ( x - p1 + 1j * p3 ) / ( p2 * sqrt2 )
+            z0 = 1j * p3 / ( p2 * sqrt2 )
+
+            vgt = p0 / wofz0 * re( wofzz )
+            dvdx = dvdz * dzdx
+
+            dvdz = p0 / wofz0 * dwdz
+            dwdz = 2j / sqrt(pi) - 2 * z * wofzz
+
+            dzdx = 1 / ( p2 * sqrt2 )
+
+
+        Parameters
+        ----------
+        xdata : array_like
+            values at which to calculate the result
+        params : array_like
+            values for the parameters.
+
+        """
+        sigma = params[2] * math.sqrt( 2.0 )
+        gamma = params[3]
+        z0 = 1j * gamma / sigma
+        z = ( xdata - params[1] ) / sigma + z0
+
+        wofz0 = numpy.real( special.wofz( z0 ) )
+        wofzz = special.wofz( z )
+        dwdz = 2j / math.sqrt( math.pi ) - 2 * z * wofzz
+
+        return numpy.real( params[0] * dwdz / ( wofz0 * sigma ) )
 
     def baseName( self ):
         """
