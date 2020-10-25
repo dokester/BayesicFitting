7b6250dd0626abc080dc0b7e55a490e58bd401bb
Do Kester <do@mac-506.local>
2020-10-25 15:40:49 +0100

    Option: fix parameters in BasicSplinesModel

--- BayesicFitting/source/BasicSplinesModel.py
+++ BayesicFitting/source/BasicSplinesModel.py
@@ -14,7 +14,7 @@ from .kernels.Tophat import Tophat
 __author__ = "Do Kester"
 __year__ = 2020
 __license__ = "GPL3"
-__version__ = "2.5.3"
+__version__ = "2.6.0"
 __url__ = "https://www.bayesicfitting.nl"
 __status__ = "Perpetual Beta"
 
